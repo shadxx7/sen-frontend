117bc2a34000427cf2c6f07397ccf654f26c1ce9
Mohamed Shadab <shadabdragon@gmail.com>
2019-04-17 22:49:52 +0530

    Fix for heroku

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const webpack = require("webpack")
 require("dotenv").config()
 
 module.exports = withCSS({
-  target: "serverless",
   webpack: config => {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env))
     return config
